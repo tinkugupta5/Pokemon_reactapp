e3d7c16dc229cf417cb871ae682b5bb28961207f
tinkugupta5 <tinku.gupta4@gmail.com>
2023-09-19 23:38:56 +0530

    changes changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({pokemon,loading}) => {
                     <div className='card'>
                         <h2>{item.id}</h2>
                         <img src={item.sprites.front_default} alt=''/>
-                        <h2>Charmender</h2>
+                        <h2>{item.name}</h2>
                     </div>
                 </>
             )
